37ed4b1c2d0de14069bcaa60353feb82a31d996c
ZygimantasL <129725364+ZygimantasL@users.noreply.github.com>
2023-06-26 13:31:38 +0300

    encryption && is page

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -6,10 +6,30 @@ type Server = {
 }
 const server = {} as Server;
 
-server.httpServer = http.createServer((req: IncomingMessage, res: ServerResponse) => {
-    console.log(req.url);
-    let responseContent = '';
+   
+    server.httpServer = http.createServer(async (req: IncomingMessage, res: ServerResponse) => {
+        console.log(req.url);
+        const socket = req.socket as any; 
+        const encryption = socket.encryption as any;
+        const ssl = encryption !== undefined ? 's' : '';
+    
+        const baseURL = `http${ssl}://${req.headers.host}`;
+        const parsedURL = new URL(req.url ?? '', baseURL);
+        const httpMethod = req.method ? req.method.toLowerCase() : 'get';
+        const trimmedPath = parsedURL.pathname
+            .replace(/^\/+|\/+$/g, '')
+            .replace(/\/\/+/g, '/');
+    
+        const textFileExtensions = ['css', 'js', 'svg', 'webmanifest'];
+        const binaryFileExtensions = ['png', 'jpg', 'jpeg', 'webp', 'ico', 'eot', 'ttf', 'woff', 'woff2', 'otf'];
+        const fileExtension = trimmedPath.slice(trimmedPath.lastIndexOf('.') + 1);
+    
+        const isTextFile = textFileExtensions.includes(fileExtension);
+        const isBinaryFile = binaryFileExtensions.includes(fileExtension);
+        const isAPI = trimmedPath.startsWith('api/');
+        const isPage = !isTextFile && !isBinaryFile && !isAPI;
 
+    let responseContent = '';
     return res.end(responseContent);
 });
 
